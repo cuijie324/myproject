5007f92f7ac7fd09da632f03e60a928105cdc313
Cui Jie <yinsu@foxmail.com>
2018-11-22 00:21:45 +0800

    django 限制用户访问
    只有已登录的用户才能访问

--- python/learning_log/learning_log/settings.py
+++ python/learning_log/learning_log/settings.py
@@ -43,6 +43,9 @@ INSTALLED_APPS = [
     'users',
 ]
 
+# 我的设置
+LOGIN_URL = '/usrs/login/'
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
--- python/learning_log/learning_logs/views.py
+++ python/learning_log/learning_logs/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
+from django.contrib.auth.decorators import login_required
 from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
 
@@ -12,6 +13,7 @@ def index(request):
     return render(request, 'learning_logs/index.html')
 
 
+@login_required
 def topics(request):
     """显示所有的主题"""
     topics = Topic.objects.order_by('date_added')
@@ -19,6 +21,7 @@ def topics(request):
     return render(request, 'learning_logs/topics.html', context)
 
 
+@login_required
 def topic(request, topic_id):
     """显示单个主题及其所有的条目"""
     topic = Topic.objects.get(id=topic_id)
@@ -27,6 +30,7 @@ def topic(request, topic_id):
     return render(request, 'learning_logs/topic.html', context)
 
 
+@login_required
 def new_topic(request):
     """添加新主题"""
     if request.method != 'POST':
@@ -41,6 +45,7 @@ def new_topic(request):
     return render(request, 'learning_logs/new_topic.html', context)
 
 
+@login_required
 def new_entry(request, topic_id):
     """在特定主题中添加新条目"""
     topic = Topic.objects.get(id=topic_id)
@@ -59,6 +64,7 @@ def new_entry(request, topic_id):
     return render(request, 'learning_logs/new_entry.html', context)
 
 
+@login_required
 def edit_entry(request, entry_id):
     """编辑既有条目"""
     entry = Entry.objects.get(id=entry_id)
