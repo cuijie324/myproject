2226a7fe75cfb36387a9f2b52ea0362b426819fa
Cui Jie <yinsu@foxmail.com>
2018-11-22 21:11:31 +0800

    django bootstrap
    使用bootstrap设置样式

--- python/learning_log/learning_log/settings.py
+++ python/learning_log/learning_log/settings.py
@@ -38,13 +38,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # 第三方应用程序
+    'bootstrap3',
+
     # 我的应用程序
     'learning_logs',
     'users',
 ]
-
-# 我的设置
-LOGIN_URL = '/usrs/login/'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -125,3 +125,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# 我的设置
+LOGIN_URL = '/usrs/login/'
+
+# django-bootstrap3的设置
+BOOTSTRAP3 = {
+    'include_jquery': True,
+}
